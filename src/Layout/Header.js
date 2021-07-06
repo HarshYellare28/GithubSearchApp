793c08d8e40548dfb9b324fdfa5f56f4234d79d0
harshyellare <yellareharsh@outlook.com>
2021-07-06 16:46:14 +0530

    modified routes

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -17,12 +17,12 @@ const Header = () => {
     
     return(
       <Navbar style={{backgroundColor:"#0D0D0D"}} expand="lg">
-        <Navbar.Brand href="#home" style={{color:"#CAD5E2",padding:"5px"}}>Github Search App</Navbar.Brand>
+        <Navbar.Brand style={{color:"#CAD5E2",padding:"5px"}}>Github Search App</Navbar.Brand>
         <Navbar.Text className="mr-auto" style={{color:"#CAD5E2"}}>{context.user?.email ? context.user?.email : ""}</Navbar.Text>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-dark"/>
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-              { context.user ? (<Nav.Link tag={Link} onClick={() => (context.setUser(null))} style={{color:"#CAD5E2",padding:"5px"}}>Log Out</Nav.Link>) : <Row><Nav.Link tag={Link} href="/SignUp" style={{color:"#CAD5E2",padding:"5px",marginLeft:"15px"}}>Sign Up</Nav.Link><Nav.Link tag={Link} href="/SignIn" style={{color:"#CAD5E2",padding:"5px",paddingLeft:"10px",marginRight:"10px"}}>Sign In</Nav.Link></Row> } 
+              { context.user ? (<Nav.Link tag={Link} onClick={() => (context.setUser(null))} style={{color:"#CAD5E2",padding:"5px"}}>Log Out</Nav.Link>) : <Row><Nav.Link tag={Link} href="https://harshyellare28.github.io/GithubSearchApp/SignUp" style={{color:"#CAD5E2",padding:"5px",marginLeft:"15px"}}>Sign Up</Nav.Link><Nav.Link tag={Link} href="https://harshyellare28.github.io/GithubSearchApp/SignIn" style={{color:"#CAD5E2",padding:"5px",paddingLeft:"10px",marginRight:"10px"}}>Sign In</Nav.Link></Row> } 
         </Nav>
         </Navbar.Collapse>
       </Navbar>
